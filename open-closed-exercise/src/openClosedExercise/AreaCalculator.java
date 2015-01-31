3c1954cca8aad4868b8811b6883ce823b10ce43e
spencerhrob <spencer.robinson@us.sios.com>
2015-01-30 20:32:46 -0500

    Updates to code structure

---
+++ open-closed-exercise/src/openClosedExercise/AreaCalculator.java
@@ -0,0 +1,20 @@
+package openClosedExercise;
+
+public class AreaCalculator {
+    public static double calculateArea(Object[] shapes) {
+        double area = 0.0;
+        
+        for (Object shape : shapes) {
+            if (shape instanceof Circle) {
+                Circle circle = (Circle)shape;
+                area += Math.PI * circle.getRadius() * circle.getRadius();
+            }
+            else if (shape instanceof Rectangle) {
+                Rectangle rectangle = (Rectangle)shape;
+                area += rectangle.getHeight() * rectangle.getWidth();
+            }
+        }
+        
+        return area;
+    }
+}
