c100ffa7fb5009606867e3da1305312c8fd2464b
TracyMarlatt <tracy.d.lemmond@gmail.com>
2015-02-03 18:13:41 -0500

    Update Main.java
    Added a new triangle object to the array of shapes and compute aggregate perimeter

--- plan-for-extension-exercise/src/planForExtensionExercise/Main.java
+++ plan-for-extension-exercise/src/planForExtensionExercise/Main.java
@@ -2,16 +2,20 @@ package planForExtensionExercise;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Circle circle = new Circle(25);
-        Rectangle rectangle = new Rectangle(10, 20);
-        Rectangle square = new Rectangle(10, 10);
-        
-        Object[] shapes = { circle, rectangle, square };
-        
-        double totalArea = AreaCalculator.calculateArea(shapes);
-        
-        System.out.println("The total area is " + totalArea + ".");
-    }
+	public static void main(String[] args) {
 
+		Circle circle = new Circle(25);
+		Rectangle rectangle = new Rectangle(10, 20);
+		Rectangle square = new Rectangle(10, 10);
+		Triangle triangle = new Triangle(3,4,5);
+		
+		Shape[] shapes = { circle, rectangle, square, triangle };
+
+		double totalArea = AreaCalculator.calculateArea(shapes);
+		System.out.println("The total area is " + totalArea + ".");
+
+		double totalPerimeter = PerimeterCalculator.calculatePerimeter(shapes);
+		System.out.println("The total perimeter is " + totalPerimeter + ".");
+
+	}
 }
