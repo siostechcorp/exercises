82f7b2d3d3c8be2d147bb438c9015894ba58b3f4
TracyMarlatt <tracy.d.lemmond@gmail.com>
2015-02-03 18:10:40 -0500

    Create Shape.java

---
+++ plan-for-extension-exercise/src/planForExtensionExercise/Shape.java
@@ -0,0 +1,9 @@
+package planForExtensionExercise;
+
+public abstract class Shape {
+
+	// Subclasses implement the following methods
+	abstract double getArea();
+	abstract double getPerimeter();
+
+}
