df0a4f2d93e5c3adc9f852ddd24ad49b436a9d6e
TracyMarlatt <tracy.d.lemmond@gmail.com>
2015-02-05 15:13:32 -0500

    Update Main.java

--- plan-for-extension-exercise/src/planForExtensionExercise/Main.java
+++ plan-for-extension-exercise/src/planForExtensionExercise/Main.java
@@ -1,17 +1,20 @@
 package planForExtensionExercise;
 
 public class Main {
+	public static void main(String[] args) {
+		Circle circle = new Circle(25);
+		Rectangle rectangle = new Rectangle(10, 20);
+		Rectangle square = new Rectangle(10, 10);
+		
+		Triangle triangle = new Triangle (3,4,5);
+		
+		Shape[] shapes = { circle, rectangle, square };
+		Shape[] shapes2 = { circle, rectangle, square, triangle };
+		double totalArea = TotalAreaCalculator.calculateArea(shapes);
+		System.out.println("The total area is " + totalArea + ".");
 
-    public static void main(String[] args) {
-        Circle circle = new Circle(25);
-        Rectangle rectangle = new Rectangle(10, 20);
-        Rectangle square = new Rectangle(10, 10);
-        
-        Object[] shapes = { circle, rectangle, square };
-        
-        double totalArea = AreaCalculator.calculateArea(shapes);
-        
-        System.out.println("The total area is " + totalArea + ".");
-    }
+		double totalArea2 = TotalAreaCalculator.calculateArea(shapes2);
+		System.out.println("The total area with triangle is " + totalArea2 + ".");
 
+	}
 }
