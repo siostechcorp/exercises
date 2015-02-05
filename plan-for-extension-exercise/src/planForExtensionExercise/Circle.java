1c4cbc74bfddfe0a8a271c90234519122cebdeeb
TracyMarlatt <tracy.d.lemmond@gmail.com>
2015-02-05 15:11:44 -0500

    Update Circle.java

--- plan-for-extension-exercise/src/planForExtensionExercise/Circle.java
+++ plan-for-extension-exercise/src/planForExtensionExercise/Circle.java
@@ -1,13 +1,21 @@
 package planForExtensionExercise;
 
-public class Circle {
-    private double radius;
-    
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-    
-    public double getRadius() {
-        return this.radius;
-    }
+public class Circle implements Shape {
+	private double radius;
+
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+	public double getRadius() {
+		return this.radius;
+	}
+
+	public double getArea() {
+		return (Math.PI * this.radius * this.radius);
+	}
+
+	public double getPerimeter() {
+		return (2 * Math.PI * this.radius);
+	}
 }
