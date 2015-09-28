b6b0374abbe4b171df996cf3d6161a1f2eb18b4d
Travis Goldie <travis.goldie@steeleye.com>
2015-09-28 16:41:19 -0400

    Again, generator

---
+++ single-page-app-exercise/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name singlePageAppExerciseApp
+ * @description
+ * # singlePageAppExerciseApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('singlePageAppExerciseApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
